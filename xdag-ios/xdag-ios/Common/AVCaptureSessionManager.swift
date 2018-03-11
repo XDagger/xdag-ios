e2b70d7620dd0db6dc893bf9866a7635d28a9980
feiin <feiin@qq.com>
2018-03-11 22:28:23 +0800

    Added AVCaptureSessionManager & AppPermissions

---
+++ xdag-ios/xdag-ios/Common/AVCaptureSessionManager.swift
@@ -0,0 +1,156 @@
+//
+//  AVCaptureSessionManager.swift
+//  xdag-ios
+//
+//  Created by yangyin on 2018/3/11.
+//  Copyright © 2018年 xdag.org. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import Photos
+
+typealias SuccessCallback = (String?) -> Void
+
+
+class AVCaptureSessionManager: AVCaptureSession {
+    
+    private lazy var device: AVCaptureDevice? = {
+        return AVCaptureDevice.default(for:.video)
+    }()
+    
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        return AVCaptureVideoPreviewLayer(session: self)
+    }()
+    
+    private var successCallback:SuccessCallback?
+    
+    convenience init(captureType: AVCaptureType, scanRect: CGRect, success: @escaping SuccessCallback) {
+        self.init()
+        successCallback = success
+        do
+        {
+            let input = try AVCaptureDeviceInput(device: device!)
+            let output = AVCaptureMetadataOutput()
+            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            
+            if !scanRect.equalTo(CGRect.null) {
+                output.rectOfInterest = scanRect
+            }
+            
+            sessionPreset = AVCaptureSession.Preset.high
+            
+            if canAddInput(input) {
+                addInput(input)
+            }
+            
+            if canAddOutput(output) {
+                addOutput(output)
+            }
+            
+            output.metadataObjectTypes = captureType.supportTypes()
+
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(stop),
+                                                   name: NSNotification.Name.UIApplicationDidEnterBackground,
+                                                   object: nil)
+            
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(start),
+                                                   name:NSNotification.Name.UIApplicationWillEnterForeground,
+                                                   object: nil)
+            
+        }
+        catch {
+            print(error)
+            return;
+        }
+    }
+    
+    /// startRunning
+    @objc func start() {
+        startRunning()
+    }
+    
+    /// stopRunning
+    @objc func stop() {
+        stopRunning()
+    }
+    
+    func scanPhoto(image: UIImage, success: SuccessCallback) {
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode,
+                                  context: nil,
+                                  options: [CIDetectorAccuracy : CIDetectorAccuracyHigh])
+        if detector != nil {
+            let features = detector!.features(in: CIImage(cgImage: image.cgImage!))
+            for temp in features {
+                let result = (temp as! CIQRCodeFeature).messageString
+                success(result)
+                return
+            }
+            success(nil)
+        }else {
+            success(nil)
+        }
+        
+    }
+    
+    func setPreviewLayerIn(view :UIView) {
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.frame = view.bounds
+        view.layer.insertSublayer(previewLayer, at: 0)
+        start()
+    }
+}
+
+
+extension AVCaptureSessionManager: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if (metadataObjects.count > 0) {
+            stop()
+            let result = metadataObjects.last as! AVMetadataMachineReadableCodeObject
+            successCallback!(result.stringValue)
+        }
+    }
+}
+
+
+enum AVCaptureType {
+    case AVCaptureTypeQRCode
+    case AVCaptureTypeBarCode
+    case AVCaptureTypeBoth
+    func supportTypes() -> [AVMetadataObject.ObjectType] {
+        switch self {
+        case .AVCaptureTypeQRCode:
+            return [AVMetadataObject.ObjectType.qr]
+        case .AVCaptureTypeBarCode:
+            return [AVMetadataObject.ObjectType.dataMatrix,
+                    AVMetadataObject.ObjectType.itf14,
+                    AVMetadataObject.ObjectType.interleaved2of5,
+                    AVMetadataObject.ObjectType.aztec,
+                    AVMetadataObject.ObjectType.pdf417,
+                    AVMetadataObject.ObjectType.code128,
+                    AVMetadataObject.ObjectType.code93,
+                    AVMetadataObject.ObjectType.ean8,
+                    AVMetadataObject.ObjectType.ean13,
+                    AVMetadataObject.ObjectType.code39Mod43,
+                    AVMetadataObject.ObjectType.code39,
+                    AVMetadataObject.ObjectType.upce]
+        case .AVCaptureTypeBoth:
+            return [AVMetadataObject.ObjectType.qr,
+                    AVMetadataObject.ObjectType.dataMatrix,
+                    AVMetadataObject.ObjectType.itf14,
+                    AVMetadataObject.ObjectType.interleaved2of5,
+                    AVMetadataObject.ObjectType.aztec,
+                    AVMetadataObject.ObjectType.pdf417,
+                    AVMetadataObject.ObjectType.code128,
+                    AVMetadataObject.ObjectType.code93,
+                    AVMetadataObject.ObjectType.ean8,
+                    AVMetadataObject.ObjectType.ean13,
+                    AVMetadataObject.ObjectType.code39Mod43,
+                    AVMetadataObject.ObjectType.code39,
+                    AVMetadataObject.ObjectType.upce]
+        }
+    }
+}
---
+++ xdag-ios/xdag-ios/Common/AppPermissions.swift
@@ -0,0 +1,73 @@
+//
+//  AppPermissions.swift
+//  xdag-ios
+//
+//  Created by yangyin on 2018/3/11.
+//  Copyright © 2018年 xdag.org. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import AVFoundation
+import Photos
+import AssetsLibrary
+
+class AppPermissions: NSObject {
+    
+    static func authorizePhotoWith(completion:@escaping (Bool)->Void )
+    {
+        let granted = PHPhotoLibrary.authorizationStatus()
+        switch granted {
+        case PHAuthorizationStatus.authorized:
+            completion(true)
+        case PHAuthorizationStatus.denied,PHAuthorizationStatus.restricted:
+            completion(false)
+        case PHAuthorizationStatus.notDetermined:
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                DispatchQueue.main.async {
+                    completion(status == PHAuthorizationStatus.authorized ? true:false)
+                }
+            })
+        }
+    }
+    
+    static func authorizeCameraWith(completion:@escaping (Bool)->Void )
+    {
+        let granted = AVCaptureDevice.authorizationStatus(for: AVMediaType.video);
+        
+        switch granted {
+        case .authorized:
+            completion(true)
+            break;
+        case .denied:
+            completion(false)
+            break;
+        case .restricted:
+            completion(false)
+            break;
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted:Bool) in
+                DispatchQueue.main.async {
+                    completion(granted)
+                }
+            })
+        }
+    }
+    
+    static func jumpToSystemSetting()
+    {
+        let appSetting = URL(string:UIApplicationOpenSettingsURLString)
+        
+        if appSetting != nil
+        {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(appSetting!, options: [:], completionHandler: nil)
+            }
+            else{
+                UIApplication.shared.openURL(appSetting!)
+            }
+        }
+    }
+    
+}
