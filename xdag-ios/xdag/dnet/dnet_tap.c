2f7847698f898ea4cae4960e642a28a683bd427d
solar <feiin@qq.com>
2018-05-08 22:02:14 +0800

    Added xdag c souces

---
+++ xdag-ios/xdag/dnet/dnet_tap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include "dnet_tap.h"
+#include <net/if.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+//#include <net/if_utun.h>
+//#include <sys/kdebug.h>
+
+int dnet_tap_open(int tap_number) {
+    return -1;
+}
